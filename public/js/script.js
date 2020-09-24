65bb2d45a78be54fc3ea8521a5d8215b3cddf8d3
Anisa <aagermanova@gmail.com>
2020-09-23 17:26:00 -0700

    css part2

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,11 @@
 $(function(){
+  $('.photos-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button class="photos-slider__btn photos-slider__btnprev"><img src="../img/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="photos-slider__btn photos-slider__btnnext"><img src="../img/arrow-right.svg" alt=""></button>',
 
 
+  });
 
-  
 });
