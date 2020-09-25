0404420e1b960885c2d1f66e9d2f8ad579f5d825
Anisa <aagermanova@gmail.com>
2020-09-24 20:05:00 -0700

    menu__btn span

--- public/js/script.js
+++ public/js/script.js
@@ -4,8 +4,10 @@ $(function(){
     slidesToScroll: 1,
     prevArrow: '<button class="photos-slider__btn photos-slider__btnprev"><img src="../img/arrow-left.svg" alt=""></button>',
     nextArrow: '<button class="photos-slider__btn photos-slider__btnnext"><img src="../img/arrow-right.svg" alt=""></button>',
+  });
 
-
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
   });
 
 });
